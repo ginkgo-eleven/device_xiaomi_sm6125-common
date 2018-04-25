LineageOS/android_hardware_qcom_display

display: Partially convert makefiles to blueprints
Change-Id: I5429ecee3985e305290e312921725460ef195b83

include/Android.mk
@@ -2,24 +2,19 @@ LOCAL_PATH:= $(call my-dir)
 include $(LOCAL_PATH)/../common.mk
 include $(CLEAR_VARS)
 
+# Legacy header copy. This is deprecated.
+# Modules using these headers should shift to using
+# LOCAL_HEADER_LIBRARIES := display_headers
 LOCAL_VENDOR_MODULE           := true
 LOCAL_COPY_HEADERS_TO         := $(common_header_export_path)
-LOCAL_COPY_HEADERS            := display_properties.h color_metadata.h
+LOCAL_COPY_HEADERS            := color_metadata.h \
+                                 display_properties.h \
+                                 ../libqdutils/qd_utils.h \
+                                 ../libqdutils/qdMetaData.h \
+                                 ../libqdutils/display_config.h \
+                                 ../libqservice/QServiceUtils.h \
+                                 ../libqservice/IQService.h \
+                                 ../libqservice/IQHDMIClient.h \
+                                 ../libqservice/IQClient.h
 
 include $(BUILD_COPY_HEADERS)
-
-include $(CLEAR_VARS)
-#TODO move all exported headers to this directory
-LOCAL_MODULE                  := display_headers
-LOCAL_VENDOR_MODULE           := true
-LOCAL_EXPORT_C_INCLUDE_DIRS   := $(LOCAL_PATH) \
-                                 $(display_top)/libcopybit \
-                                 $(display_top)/libdrmutils \
-                                 $(display_top)/libqdutils \
-                                 $(display_top)/libqservice \
-                                 $(display_top)/gpu_tonemapper \
-                                 $(display_top)/sdm/include \
-                                 $(display_top)/gralloc \
-                                 $(display_top)/libgralloc1
-
-include $(BUILD_HEADER_LIBRARY)

ginkgo-eleven/device_xiaomi_sm6125-common

include/Android.mk
@@ -2,26 +2,19 @@ LOCAL_PATH:= $(call my-dir)
 include $(LOCAL_PATH)/../common.mk
 include $(CLEAR_VARS)
 
+# Legacy header copy. This is deprecated.
+# Modules using these headers should shift to using
+# LOCAL_HEADER_LIBRARIES := display_headers
 LOCAL_VENDOR_MODULE           := true
 LOCAL_COPY_HEADERS_TO         := $(common_header_export_path)
-LOCAL_COPY_HEADERS            := color_metadata.h
-LOCAL_COPY_HEADERS            += display_color_processing.h
+LOCAL_COPY_HEADERS            := color_metadata.h \
+                                 display_color_processing.h \
+                                 ../libqdutils/qd_utils.h \
+                                 ../libqdutils/qdMetaData.h \
+                                 ../libqdutils/display_config.h \
+                                 ../libqservice/QServiceUtils.h \
+                                 ../libqservice/IQService.h \
+                                 ../libqservice/IQHDMIClient.h \
+                                 ../libqservice/IQClient.h
 
 include $(BUILD_COPY_HEADERS)
-
-include $(CLEAR_VARS)
-#TODO move all exported headers to this directory
-LOCAL_MODULE                  := display_headers
-LOCAL_VENDOR_MODULE           := true
-LOCAL_EXPORT_C_INCLUDE_DIRS   := $(LOCAL_PATH) \
-                                 $(display_top)/libcopybit \
-                                 $(display_top)/libdrmutils \
-                                 $(display_top)/libqdutils \
-                                 $(display_top)/libqservice \
-                                 $(display_top)/gpu_tonemapper \
-                                 $(display_top)/sdm/include \
-                                 $(display_top)/gralloc \
-                                 $(display_top)/libgralloc1 \
-                                 $(display_top)/libdebug
-
-include $(BUILD_HEADER_LIBRARY)