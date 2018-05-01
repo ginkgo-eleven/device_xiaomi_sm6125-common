LineageOS/android_hardware_qcom_display

hwc2: Do not mark single buffered or color fill layers as skip
CRs-Fixed: 2225117
Change-Id: I9d924a5b16c7755ca9fb7f4cf799328456fe11c6

sdm/libs/hwc2/hwc_layers.cpp
@@ -416,6 +416,9 @@ HWC2::Error HWCLayer::SetLayerSourceCrop(hwc_frect_t crop) {
                               (crop.top != roundf(crop.top)) ||
                               (crop.right != roundf(crop.right)) ||
                               (crop.bottom != roundf(crop.bottom)));
+  if (non_integral_source_crop_) {
+    DLOGV_IF(kTagClient, "Crop: LRTB %f %f %f %f", crop.left, crop.top, crop.right, crop.bottom);
+  }
   if (layer_->src_rect != src_rect) {
     geometry_changes_ |= kSourceCrop;
     layer_->src_rect = src_rect;

ginkgo-eleven/device_xiaomi_sm6125-common

sdm/libs/hwc2/hwc_layers.cpp
@@ -439,6 +439,9 @@ HWC2::Error HWCLayer::SetLayerSourceCrop(hwc_frect_t crop) {
                               (crop.top != roundf(crop.top)) ||
                               (crop.right != roundf(crop.right)) ||
                               (crop.bottom != roundf(crop.bottom)));
+  if (non_integral_source_crop_) {
+    DLOGV_IF(kTagClient, "Crop: LTRB %f %f %f %f", crop.left, crop.top, crop.right, crop.bottom);
+  }
   if (layer_->src_rect != src_rect) {
     geometry_changes_ |= kSourceCrop;
     layer_->src_rect = src_rect;