AOSiP-Devices/device_xiaomi_sdm660-common

sdm660-common: create libGLESv2_adreno.so symlink
* libGLESv2_adreno.so is needed on camera blobs

Change-Id: Iced972a02073e0c327e02a359870aeec299af0bd

Android.mk
@@ -108,5 +108,12 @@ ALL_DEFAULT_INSTALLED_MODULES += \
     $(RFS_MSM_MPSS_SYMLINKS) \
     $(RFS_MSM_SLPI_SYMLINKS)
 
+EGL_SYMLINK := $(TARGET_OUT_VENDOR)/lib/libEGL_adreno.so
+$(EGL_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf egl/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(EGL_SYMLINK)
+
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif

ginkgo-eleven/device_xiaomi_sm6125-common

Android.mk
@@ -108,5 +108,12 @@ ALL_DEFAULT_INSTALLED_MODULES += \
     $(RFS_MSM_MPSS_SYMLINKS) \
     $(RFS_MSM_SLPI_SYMLINKS)
 
+EGL_SYMLINK := $(TARGET_OUT_VENDOR)/lib/libGLESv2_adreno.so
+$(EGL_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf egl/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(EGL_SYMLINK)
+
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif