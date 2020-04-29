xiaomi-sdm660-devs/android_device_xiaomi_sdm660-common

sdm660-common: My name is lavender
Change-Id: Ie820810a928378a194bfa20fdae69385d1683dc7

Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter jasmine_sprout jason twolip wayne,$(TARGET_DEVICE)),)
+ifneq ($(filter jasmine_sprout jason twolip wayne lavender,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 

setup-makefiles.sh
@@ -39,7 +39,7 @@ fi
 setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
 
 # Copyright headers and guards
-write_headers "jasmine_sprout jason twolip wayne"
+write_headers "jasmine_sprout jason twolip wayne lavender"
 
 write_makefiles "$MY_DIR"/proprietary-files.txt true
 

ginkgo-eleven/device_xiaomi_sm6125-common

Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter jasmine_sprout jason twolip wayne,$(TARGET_DEVICE)),)
+ifneq ($(filter jasmine_sprout jason lavender twolip wayne,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 

setup-makefiles.sh
@@ -39,7 +39,7 @@ fi
 setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
 
 # Copyright headers and guards
-write_headers "jasmine_sprout jason twolip wayne"
+write_headers "jasmine_sprout jason lavender twolip wayne"
 
 write_makefiles "$MY_DIR"/proprietary-files.txt true
 